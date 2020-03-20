d54a838b6a18eb71273441d5ae0fdb4b10463cf0
emmynice <emmynice96@gmail.com>
2020-03-20 02:15:09 +0100

    deploy

---
+++ script.js
@@ -0,0 +1,26 @@
+var userName = "Daniel Emmanuel Enyojo";
+var myCourses = ['HTML', 'CSS','JAVASCRIPT','PHP','NODE JS'];
+
+console.log("Name:" + userName);
+console.log(myCourses);
+if(myCourses.length%2 !=0){
+	var n;
+for(n=1;n<=200; n++){
+	if((n%2)!=0){
+		console.log(n);
+	}
+}
+
+}
+if(myCourses.length%2 ==0){
+	var n;
+for(n=1;n<=200; n++){
+	if((n%2)==0){
+		console.log(n);
+	}
+}
+
+}
+
+
+
